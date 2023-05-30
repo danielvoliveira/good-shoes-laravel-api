d64b94c7dd7f257cea521a75701ce936db267cc1
Daniel Oliveira <danielvitol@hotmail.com>
2023-05-30 19:10:38 -0300

    adjusting seeds file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            //UserSeeder::class,
+            UserSeed::class,
         	VendedorSeeder::class,
             ClienteSeeder::class,
             CorSeeder::class,
